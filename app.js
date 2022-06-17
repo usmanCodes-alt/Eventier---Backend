f93bb2fe22ef43d3916aacebd0d29c53eb28bb85
usmanCodes-alt <usman.ghani4501@gmail.com>
2022-06-18 03:27:48 +0500

    added exist log

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
 const router = require("./api/routes/routes");
+const fs = require("fs");
 require("./api/utils/sentiment");
+
+console.log(fs.existsSync(__dirname, "./images/service-images"));
+console.log(fs.existsSync(__dirname, "./images/profile-pictures"));
 
 const app = express();
 const port = process.env.PORT || 3000;
